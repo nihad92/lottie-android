d93c679b2e6cdb9fafc0b289f3d8cf38a73beca6
Nihad Ahmetovic <nihad92@gmail.com>
2020-02-13 02:50:03 +0100

    Removed check for context in font asset manager if delegate is provided

--- lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
+++ lottie/src/main/java/com/airbnb/lottie/LottieDrawable.java
@@ -1060,7 +1060,7 @@ public class LottieDrawable extends Drawable implements Drawable.Callback, Anima
   }
 
   private FontAssetManager getFontAssetManager() {
-    if (getCallback() == null) {
+    if (getCallback() == null && fontAssetDelegate == null) {
       // We can't get a bitmap since we can't get a Context from the callback.
       return null;
     }
